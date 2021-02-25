d546f5dbb3f1572948cfed46c21e79ca87e721dc
IlayTissona <ilay211@gmail.com>
2021-02-25 13:11:16 +0200

    created Deck class
    constructor for full deck

--- client/utiles.js
+++ client/utiles.js
@@ -12,3 +12,31 @@ class player {
     this.points = 0;
   }
 }
+
+class Deck extends Array {
+  constructor() {
+    super();
+    for (let i = 0; i < 13; i++) {
+      for (let j = 0; j < 4; j++) {
+        let card = new Card(null, i + 1);
+        switch (j) {
+          case 0:
+            card.type = "hearts";
+            break;
+          case 1:
+            card.type = "diamonds";
+            break;
+          case 2:
+            card.type = "spades";
+            break;
+          case 3:
+            card.type = "clubs";
+            break;
+        }
+        this.push(card);
+      }
+    }
+    this.push(new Card("red", "joker"));
+    this.push(new Card("black", "joker"));
+  }
+}
